57418bbebc3c1905513ad3bfca5962191fa899aa
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2021-01-30 19:47:00 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/gabriel/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

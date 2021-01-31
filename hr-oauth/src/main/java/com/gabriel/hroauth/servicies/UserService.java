e4b2e5985767ebe41e2849e015ebc3ddce75e490
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2021-01-31 13:56:33 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/com/gabriel/hroauth/servicies/UserService.java
+++ hr-oauth/src/main/java/com/gabriel/hroauth/servicies/UserService.java
@@ -2,13 +2,16 @@ package com.gabriel.hroauth.servicies;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.hroauth.entities.User;
 import com.gabriel.hroauth.feignClients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 
@@ -22,6 +25,18 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email found: " + emal );
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		//Código repedido apenas para fins de aprendizado
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username );
 		return user;
 	}
 }

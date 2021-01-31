2becafeb01d0c1e115fac2850f40f771a6facd3f
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2021-01-31 16:52:14 -0300

    db config server

--- hr-worker/src/main/java/com/gabriel/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/gabriel/hrworker/resources/WorkerResource.java
@@ -22,9 +22,6 @@ public class WorkerResource {
 
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
-
 	@Autowired
 	private Environment env;
 
@@ -33,7 +30,7 @@ public class WorkerResource {
 
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs (){
-		logger.info("Config = " + testConfig);
+	//	logger.info("Config = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 

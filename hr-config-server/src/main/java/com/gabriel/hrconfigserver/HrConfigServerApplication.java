1e3a244fe39a39a2ba4d80794dce1093a2db4c88
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2021-02-04 15:02:49 -0300

    hr-worker Postgres dev profile

--- hr-config-server/src/main/java/com/gabriel/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/gabriel/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.gabriel.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,6 +8,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
+	
+	@Value("${spring.cloud.config.server.git.username}")
+	private String username;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
